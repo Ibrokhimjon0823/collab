947e543a3b9c86ed85f7bf75df25a0fe744ad35e
madiyor5552 <madiyor5552@gmail.com>
2022-06-15 12:13:16 +0500

    notification by email, request status change, and service cost has been added

--- main/views/service.py
+++ main/views/service.py
@@ -1,13 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from django_filters.rest_framework import DjangoFilterBackend
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+from core.utils import send_registration_email
 from main.models import Request
 from users import permissions as user_permissions
 from rest_framework.permissions import IsAuthenticated
 from main import models, filters
-from main.serailizers import company, service, RequestSerializer
+from main.serailizers import company, service, RequestSerializer, StatusChangeSerializer
 from users.admin import CustomUser
 
 
@@ -50,4 +53,20 @@ class RequestViewSet(ModelViewSet):
     #     else:
     #         return self.get_queryset().filter(companies__in=[self.request.user.company])
 
+    @action(methods=['POST'], detail=True, url_name='status-change', url_path='status-change')
+    def status_change(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = StatusChangeSerializer(
+            instance=instance,
+            data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+        request_status = serializer.validated_data.get('status')
+        serializer.save()
+        if request_status == Request.Status.APPROVED:
+            message = f"Your request has been {request.data.get('status')}. The total cost of service is {instance.total_cost} "
+        else:
+            message = f"Your request has been {request.data.get('status')}"
+        send_registration_email(instance.customer.email, message)
+        return Response({"message":"Request Status has been changed!"},status=status.HTTP_200_OK)
 
